49467540080a9f6783b7c499adaaa6cc924ffd8a
Rafael Figueiredo <rafaelsfigueiredo1@gmail.com>
2024-04-24 20:00:22 -0300

    commit com otimizações no código e descrição do desafio no arquivo README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# dio-desafio-sistema-bancario-python
+# Desafio Sistema Bancário Python
+
+O presente repositório contém um programa simples escrito em Python para desempenhar as operações de saque, depósito e extrato em um sistema bancário.
+
+Por ser um desafio simples e introdutório à linguagem de programação, não é feita verificação de usuário, agência ou número de conta.
+
+## Regras do desafio
+- Os valores informados para as operações de saque e depósito devem ser positivos;
+- Não é permitido sacar um valor superior ao saldo da conta;
+- O limite de saque é de R$ 500,00 por operação, havendo um limite de 3 saques diários;
+- Os registros de saque e depósito devem ser exibidos quando o extrato é solicitado.
--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,5 +1,8 @@
-saldo = 0
+saldo = 0.00
+LIMITE = 500.00
+NUMERO_MAXIMO_SAQUES = 3
 contador_saques = 0
+extrato = 11*"=" + " EXTRATO " + 11*"=" + "\n"
 
 menu = """
 Digite [s] para Saque
@@ -10,37 +13,55 @@ Digite [q] para Encerrar o atendimento
 """
 
 while True:
-
     operacao = input(menu)
 
     if operacao == "s":
 
-        if contador_saques < 3:
+        if contador_saques < NUMERO_MAXIMO_SAQUES:
 
             valor_saque = float(input("Insira o valor a ser sacado: R$ "))
 
-            if valor_saque <= 500.00:
-                saldo -= valor_saque
-                print(f"Seu novo saldo: R$ {saldo:.2f}")
-                contador_saques += 1
-            else:
-                print(f"Operação recusada. Saques estão limitados a R$ 500.00 por operação.")
+            limite_excedido = valor_saque > LIMITE
 
+            saldo_excedido = valor_saque > saldo
+
+            saque_negativo = valor_saque < 0
+            
+            if limite_excedido:
+                print(f"Operação recusada. Seu limite de saque é de R$ {LIMITE:.2f} por operação.")
+            elif saldo_excedido:
+                print("Operação recusada. Você não pode sacar uma quantia superior ao seu saldo.")
+            elif saque_negativo:
+                print(f"Operação recusada. Valor sacado deve ser positivo.")
+            else:
+                saldo -= valor_saque
+                extrato += f"\nSaque: R$ {valor_saque:.2f}"
+                contador_saques += 1
+        
         else:
-            print("Você atingiu o número máximo de 3 saques diários.")
+            print(f"Operação recusada. Número máximo de {NUMERO_MAXIMO_SAQUES} saques diários foi atingido.")
 
     # Depósito
     elif operacao == "d":
-
         valor_deposito = float(input("Insira valor a ser depositado: R$ "))
-        saldo += valor_deposito
-        print(f"Seu novo saldo: R$ {saldo:.2f}")
+
+        if valor_deposito > 0:
+            saldo += valor_deposito
+            extrato += f"\nDepósito: R$ {valor_deposito:.2f}"
+        else:
+            print("Operação recusada. Valor depositado deve ser positivo.")
 
     # Extrato
     elif operacao == "e":
-
-        print(f"Seu saldo: R$ = {saldo:.2f}")
+        print(extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("\n" + 31*"=" + "\n")
 
     # Encerrar o atendimento
     elif operacao == "q":
+        print("Agradecemos pela sua preferência. Tenha um ótimo dia!")
         break
+
+    # Mensagem de comando inválido
+    else:
+        print("Comando inválido. Por favor, leia as opções novamente e escolha uma operação.")
